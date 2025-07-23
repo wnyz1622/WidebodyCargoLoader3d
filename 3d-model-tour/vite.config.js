0c2e3f1d4747a2bf21e47076b6c74876ccddf10a
wnzyhsu <wnyzhsu@gmail.com>
2025-07-23 15:26:30 -0500

    Fix Vite root path for Netlify

--- 3d-model-tour/vite.config.js
+++
@@ -1,5 +0,0 @@
-// vite.config.js
-// vite.config.js
-export default {
-    base: './' // 
-  };
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '3d-model-tour',
+  build: {
+    outDir: '../dist',       // Put output at /dist in root
+    emptyOutDir: true
+  }
+});

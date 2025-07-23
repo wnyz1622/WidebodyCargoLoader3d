7848abf8df5911cf9529634c7e4ff4d19edaf05a
wnzyhsu <wnyzhsu@gmail.com>
2025-07-23 15:54:16 -0500

    downgrade rollup

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '3d-model-tour',
   build: {
-    outDir: '../dist',       // Put output at /dist in root
-    emptyOutDir: true,
-    rollupOptions: {
-      external: ['@rollup/rollup-linux-x64-gnu']
-    }
+    outDir: '../dist',
+    emptyOutDir: true
   }
 });

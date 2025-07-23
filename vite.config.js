a0eec21a4b955a8eda3169d5345f7bc8794e2672
wnzyhsu <wnyzhsu@gmail.com>
2025-07-23 15:48:12 -0500

    edit vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ export default defineConfig({
   root: '3d-model-tour',
   build: {
     outDir: '../dist',       // Put output at /dist in root
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ['@rollup/rollup-linux-x64-gnu']
+    }
   }
 });
